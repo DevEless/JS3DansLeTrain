be0fbc1d90308d70cd446bf32ed576cd7cd5fc03
DevEless <qdelameilleure@gmaile.com>
2022-12-20 10:08:27 +0100

    test

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+let nombre = 8.56;
+
+Math.round(nombre);
+Math.ceil(nombre);
+Math.floor(nombre);
+Math.trunc(nombre);
+Math.random();
+Math.floor(Math.random() * 100);
+Math.floor(Math.random() * 101);
+Math.pow(8, 2);
+Math.sqrt(9);
+Math.abs(-1);
+Math.max(-2, 1000, 8, 57);
+Math.min(-2, 1000, 8, 57);
+//
+
