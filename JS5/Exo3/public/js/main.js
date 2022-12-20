cfc628d7132ce65bb53e28cba925226f656de99d
DevEless <qdelameilleure@gmaile.com>
2022-12-20 14:23:16 +0100

    exo3

--- JS5/Exo3/public/js/main.js
+++ JS5/Exo3/public/js/main.js
@@ -1,28 +1,16 @@
+let valeur = prompt("Saisissez une valeur :");
 
-let jour = prompt("Quel jour de la semaine es-tu ?");
 
-switch (jour) {
-    case "lundi":
-        console.log("Tu as cours à MolenGeek et tu as sport en soirée.");
+switch (true) {
+    case (valeur % 2 === 0):
+
+        alert("Le nombre est un multiple de 2");
         break;
-    case "mardi":
-        console.log("Tu as réunion de travail le matin et yoga le soir avec l'autre meuf la.");
-        break;
-    case "mercredi":
-        console.log("Tu as cours de danse le matin et un rendez-vous chez le médecin l'après-midi.");
-        break;
-    case "jeudi":
-        console.log("Tu as soirée jeux en ligne avec tes amis.");
-        break;
-    case "vendredi":
-        console.log("Tu as rendez-vous au salon de coiffure et sortie cinéma avec tes collègues.");
-        break;
-    case "samedi":
-        console.log("Tu as match de foot le matin et barbecue avec ta famille l'après-midi.");
-        break;
-    case "dimanche":
-        console.log("Tu as promenade en forêt avec ton chien et après-midi lecture au calme.");
+    case (isNaN(valeur)):
+
+        alert("Ceci n'est pas un nombre");
         break;
     default:
-        console.log("Je ne connais pas ce jour de la semaine.");
+
+        alert("Le nombre n'est pas un multiple de 2");
 }
